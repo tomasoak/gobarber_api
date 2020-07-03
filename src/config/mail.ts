b920f3274987a8664db67c06ff2ff960ed666d80
Tomás Carvalho <tomas.jpeg@gmail.com>
2020-07-03 15:56:35 -0300

    setting mails from AWS SES

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'tomas.oak23@gmail.com',
+      name: 'Tomás Oak',
+    },
+  },
+} as IMailConfig;

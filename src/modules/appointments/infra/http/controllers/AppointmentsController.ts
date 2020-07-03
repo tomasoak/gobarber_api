a18ad722cfb09fe027731f146d00e7193650f5d5
Tomás Carvalho <tomas.jpeg@gmail.com>
2020-07-03 11:18:19 -0300

    removing parsedDate

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -7,17 +6,13 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 export default class AppointmentsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
-    console.log('user_id', request.user.id);
     const { provider_id, date } = request.body;
-    console.log('request_body', request.body);
-
-    const parsedDate = parseISO(date);
 
     const createAppointment = container.resolve(CreateAppointmentService);
 
     const appointment = await createAppointment.execute({
-      date: parsedDate,
       provider_id,
+      date,
       user_id,
     });
 
